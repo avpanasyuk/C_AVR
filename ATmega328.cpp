c886163ddc9355847c0315372da19bd46a25418d
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-01-12 08:22:03 -0500

    Added ATMEGA328

---
+++ ATmega328.cpp
@@ -0,0 +1,20 @@
+/*
+ * ATmega328.cpp
+ *
+ * Created: 7/12/2014 1:37:44 PM
+ *  Author: panasyuk
+ */
+
+#ifdef __AVR_ATmega328__
+#include "ATmega328.h"
+
+UART_INIT(0,0)
+UART_INIT(1,1)
+
+constexpr uint8_t Timer0Regs::Prescalers[];
+constexpr uint8_t Timer1Regs::Prescalers[];
+constexpr uint8_t Timer2Regs::Prescalers[];
+constexpr uint8_t Timer3Regs::Prescalers[];
+
+#endif
+
---
+++ ATmega328.h
@@ -0,0 +1,29 @@
+/*!
+* @file AVP_LIBS/AVR/ATmega328.h
+*
+* Created: 6/14/2014 8:24:01 PM
+*  Author: panasyuk
+* Defines TimerX static class for all timers present in the processor.
+*/
+
+
+#ifndef ATMEGA328P_TIMERS_H_
+#define ATMEGA328P_TIMERS_H_
+
+#ifndef __AVR_ATmega328__
+#error Invalid for this processor!
+#endif
+
+#include "HW_Timer.h"
+
+TIMER_DEFS(0,8,0, {0,3,6,8,10})
+TIMER_DEFS(1,16,0, {0,3,6,8,10})
+TIMER_DEFS(2,8,0, {0,3,5,6,7,8,10})
+TIMER_DEFS(3,16,1, {0,3,6,8,10})
+
+#include "HW_UART.h"
+UART_DEF(0,0,0)
+UART_DEF(1,1,1)
+
+
+#endif /* ATMEGA328P_TIMERS_H_ */
