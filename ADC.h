944c1f74786766b99d75c9bd727d628ac018ffb1
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-10-05 15:47:06 -0400

    Revert "Changes of bit functions if General lib for no good reason"
    This reverts commit 756c94a19ee3366106583fa4ebd8325086cba254.

--- ADC.h
+++ ADC.h
@@ -28,12 +28,12 @@ namespace avp {
 
     // ********* ADMUX *************
     enum Reference { AREF, AVCC, INTERNAL1_1V /* later two need external cap on AREF pin */,  };
-    inline void SelectReference( enum Reference Ref ) {  avp::setbits(ADMUX, Ref, 2, REFS0); }
+    inline void SelectReference( enum Reference Ref ) {  avp::setbits(ADMUX, REFS0, 2, Ref); }
 
     //! how 10 bits are adjusted in 16-bits output
     inline void SetLeftAdjust(bool Set=true) { avp::setbit(ADMUX,ADLAR,Set?1:0); }
     enum Channel { CH0 /* from 0 to 7 */,  TEMP=8, REF_1_1V=14, GND=15 };
-    inline void SelectChannel( uint8_t /* enum Channel */ Ch ) { avp::setbits(ADMUX, Ch, 4, MUX0); }
+    inline void SelectChannel( uint8_t /* enum Channel */ Ch ) { avp::setbits(ADMUX, MUX0, 4, Ch); }
 
     // ********* ADCSRA ************
     // This is major disable, first conversion after Enable takes longer
@@ -46,12 +46,12 @@ namespace avp {
     // do not forget global interrupt enable sei()
     inline void EnableInterrupts() { avp::set_high(ADCSRA,ADIE); }
     inline void DisableInterrupts() { avp::set_low(ADCSRA,ADIE); }
-    inline void SetPrescaler() { avp::setbits(ADCSRA,avp::CeilLog2Ratio(F_CPU,200000UL),3,ADPS0); }
+    inline void SetPrescaler() { avp::setbits(ADCSRA,ADPS0,3,avp::CeilLog2Ratio(F_CPU,200000UL)); }
 
 
     // ******** ADCSRB *************
     enum AutoTriggerSource { FREE, AN_COMPARATOR, EXT_INT_0, TIMER0_COMPA, TIMER0_OVR,  TIMER1_COMPB, TIMER1_OVR, TIMER1_CAPT };
-    inline void SetAutoTriggerSource( enum AutoTriggerSource Src ) { avp::setbits( ADCSRB, Src, 3, ADTS0); }
+    inline void SetAutoTriggerSource( enum AutoTriggerSource Src ) { avp::setbits( ADCSRB, ADTS0, 3, Src); }
 
 
     // ******** SERVICE ************
--- GIOpins.h
+++ GIOpins.h
@@ -13,43 +13,27 @@
 #include <AVP_LIBS/General/BitBang.h>
 #include <AVP_LIBS/General/Macros.h>
 
-// the difference between peading PORT address and PIN address is that PORT address is
-// reading port register which reflected in pins only when configured as output, PIN address reads pins
-
 namespace avp {
   enum PinModes {INPUT, OUTPUT};
   enum PinLevels {LOW, HIGH};
 
-  template<uint16_t DDRaddr, uint16_t PORTaddr, uint16_t PINaddr, char Name_> struct IO_Port {
-    static volatile uint8_t &port_byte() { return *(volatile uint8_t *)PORTaddr; }
-    static volatile uint8_t &pin_byte() { return *(volatile uint8_t *)PINaddr; }
-    static volatile uint8_t &mode_byte() { return *(volatile uint8_t *)DDRaddr; }
-    static void config(uint8_t IsOutput, uint8_t level) { mode_byte() = IsOutput; port_byte() = level; }
-    static void setbits(uint8_t values, uint8_t mask) { avp::setbits(port_byte(),mask,values); }
-    static void setbits(uint8_t values, uint8_t numbits, uint8_t lowest_bit = 0) {
-      avp::setbits(port_byte(),values,numbits,lowest_bit);
-    }
-    static constexpr char Name() { return Name_; }
-  };
-
-  /// @tparam Port is of class IO_Port
-  /// @tparam Number is 0-based pin number
-  template<class Port, uint8_t Number> struct Pin {
-    static constexpr char Name[] = {Port::Name,'0'+Number,0};
+  template<uint16_t DDRaddr, uint16_t PORTaddr, uint16_t PINaddr, uint8_t Number, char Port> struct Pin {
+    static constexpr char Name[] = {Port,'0'+Number,0};
     /*! sets PULLUP when mode is INPUT */
-    static void set_high() { avp::set_high(Port::port_byte(),Number); }
-    static void set_low() { avp::set_low(Port::port_byte(),Number); }
-    static void write(bool value) { avp::setbit(Port::mode_byte(),Number,value); }
-    static bool read() { return avp::getbit(Port::read(), Number); }
-    static void mode(bool IsOutput) { avp::setbit(Port::mode_byte(),Number,IsOutput); }
+    static void write(bool value) { avp::setbit(*(volatile uint8_t *)PORTaddr,Number,value );  }
+    static void set_high() { avp::set_high(*(volatile uint8_t *)PORTaddr,Number); }
+    static void set_low() { avp::set_low(*(volatile uint8_t *)PORTaddr,Number); }
+    static bool read() { return avp::getbit(*(volatile uint8_t *)PINaddr,Number); }
+    static void mode(bool IsOutput) { avp::setbit(*(volatile uint8_t *)DDRaddr,Number,IsOutput); }
     static void config(bool IsOutput, bool level) { mode(IsOutput); write(level); }
-    static void toggle() { avp::set_high(Port::pin_byte(),Number); /* just works this way, according to spec */   }
+    static void toggle() { avp::toggle(*(volatile uint8_t *)PORTaddr,Number);  }
   }; // Pin
 
-  template<class Port, uint8_t Number> constexpr char Pin<Port,Number>::Name[];
+  template<uint16_t DDRaddr, uint16_t PORTaddr, uint16_t PINaddr, uint8_t Number, char Port>
+  constexpr char Pin<DDRaddr,PORTaddr,PINaddr,Number,Port>::Name[];
 }; // avp
 
-#define AVP_PORT(PortChar) avp::IO_Port<uint16_t(&COMB2(DDR,PortChar)),uint16_t(&COMB2(PORT,PortChar)),uint16_t(&COMB2(PIN,PortChar)),(#PortChar)[0]>
-#define AVP_PIN(Port,Number) avp::Pin<AVP_PORT(Port),Number>
+#define AVP_PIN(Port,Number) \
+  avp::Pin<uint16_t(&COMB2(DDR,Port)),uint16_t(&COMB2(PORT,Port)),uint16_t(&COMB2(PIN,Port)),Number,(#Port)[0]>
 
 #endif /* GIOPINS_H_ */
