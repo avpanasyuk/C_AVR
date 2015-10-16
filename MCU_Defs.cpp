c39524a513a8e4c542abe97eb8bcad230ea385f6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-10-16 10:27:43 -0400

    Minor

--- MCU_Defs.cpp
+++ MCU_Defs.cpp
@@ -7,31 +7,24 @@
 
 #include "MCU_Defs.h"
 
+constexpr uint8_t Timer0Regs::Prescalers[];
+constexpr uint8_t Timer1Regs::Prescalers[];
+constexpr uint8_t Timer2Regs::Prescalers[];
+
 #if defined(__AVR_ATmega1284__)
 
 UART_INIT(0,0)
 UART_INIT(1,1)
 
-constexpr uint8_t Timer0Regs::Prescalers[];
-constexpr uint8_t Timer1Regs::Prescalers[];
-constexpr uint8_t Timer2Regs::Prescalers[];
 constexpr uint8_t Timer3Regs::Prescalers[];
 
 #elif defined(__AVR_ATmega328P__)
 
 UART_INIT(0,)
 
-constexpr uint8_t Timer0Regs::Prescalers[];
-constexpr uint8_t Timer1Regs::Prescalers[];
-constexpr uint8_t Timer2Regs::Prescalers[];
-
 #elif defined(__AVR_ATmega88A__)
 
 UART_INIT(0,)
-
-constexpr uint8_t Timer0Regs::Prescalers[];
-constexpr uint8_t Timer1Regs::Prescalers[];
-constexpr uint8_t Timer2Regs::Prescalers[];
 
 #endif
 
