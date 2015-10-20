5a6af1b613c2476ca0c9f487f6bbdc3aa3d5523b
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-10-19 22:08:32 -0400

    minoe

--- EEPROM.h
+++ EEPROM.h
@@ -11,44 +11,47 @@
 
 #include <stddef.h>
 #include <avr/io.h>
-#include "service.h"
+#include <AVP_LIBS/General/Error.h>
 
 namespace EEPROM {
-  static inline void write(uint16_t address, uint8_t value) {
+  static inline void write_byte(uint16_t address, uint8_t value) {
     /* Wait for completion of previous write */
-    while(EECR & (1<<EEPE));
-    /* Set up address and Data Registers */
+    debug_printf("%hu<-%hhu.",address,value);
+
+    ISR_Blocker Auto;
+    while(EECR & ((1<<EEPE) | (1<<EEMPE)));
+    // while(SPMCSR & (1<<SPMEN));
     EEAR = address;
     EEDR = value;
 
-    ISR_Blocker Auto;
-    /* Write logical one to EEMPE */
     EECR |= (1<<EEMPE);
-    /* Start eeprom write by setting EEPE */
     EECR |= (1<<EEPE);
   } // write
 
-  static inline uint8_t read(uint16_t address) {
+  static inline uint8_t read_byte(uint16_t address) {
     /* Wait for completion of previous write */
+    ISR_Blocker Auto;
+
     while(EECR & (1<<EEPE));
-    /* Set up address register */
     EEAR = address;
-    /* Start eeprom read by writing EERE */
+
     EECR |= (1<<EERE);
-    /* Return data from Data Register */
+    debug_printf("%hu->%hhu.",address,EEDR);
     return EEDR;
   } // read
 
-  template<typename T> void write(uint16_t address, T const *from, size_t n) {
-    uint8_t const *p_ = (uint8_t const *)from, *AfterEnd = p_ + sizeof(T)*n;
+  /// @tparam T. If T is array the size is the whole array, n should be 1
+  template<typename T> void write_object(uint16_t address, T const &from, size_t n=1) {
+    uint8_t const *p_ = (uint8_t const *)&from, *AfterEnd = p_ + sizeof(T)*n;
 
-    while(p_ < AfterEnd) write(address++,*(p_++));
+    while(p_ < AfterEnd) write_byte(address++,*(p_++));
   }
 
-  template<typename T> void read(uint16_t address, T *to, size_t n) {
-    uint8_t *p_ = (uint8_t *)to, *AfterEnd = p_ + sizeof(T)*n;
+  /// @tparam T. If T is array the size is the whole array, n should be 1
+  template<typename T> void read_object(uint16_t address, T &to, size_t n=1) {
+    uint8_t *p_ = (uint8_t *)&to, *AfterEnd = p_ + sizeof(T)*n;
 
-    while(p_ < AfterEnd) *(p_++) = read(address++);
+    while(p_ < AfterEnd) *(p_++) = read_byte(address++);
   }
 } // namespace EEPROM
 
